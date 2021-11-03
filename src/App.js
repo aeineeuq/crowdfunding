c3c95989ea510870028d8db3575597b26db7058b
qambatali <q.ambatali@gmail.com>
2021-11-03 23:11:30 +1000

    Linked css to app

--- src/App.js
+++ src/App.js
@@ -1,26 +1,37 @@
 import React from "react";
-import { BrowserRouter as Router, Switch, Route }from"react-router-dom";
 import "./App.css";
-import Nav from "./components/Nav/Nav";import HomePage from "./pages/HomePage";
-import ProjectPage from "./pages/ProjectPage";
+import "./App.css";
+import { BrowserRouter as Router, Switch, Route }from"react-router-dom";
+import Nav from "./components/Nav/Nav";
+import HomePage from "./pages/HomePage";
 import LoginPage from "./pages/LoginPage";
+import LogoutPage from "./pages/LogoutPage";
+import ProjectPage from "./pages/ProjectPage";
+import UserPage from "./pages/UserPage";
+import EditProjectPage from './pages/EditProjectPage';
 
 function App() {
   return (
   <Router>
-  <div>
-   <Nav />
+   <div>
+     <Nav />
    
-   <Switch>
-      <Route path="/project/:id">
-      <ProjectPage />
-      </Route>
-      <Route path="/login">
-      <LoginPage />
-      </Route>
-      <Route exact path="/">
-      <HomePage />
-      </Route>
+    <Switch>
+        <Route path="/project/:id">
+          <ProjectPage />
+        </Route>
+        <Route path="/users/:id">
+          <UserPage />
+        </Route>
+        <Route path="/login">
+          <LoginPage />
+        </Route>
+        <Route path="/logout">
+          <LogoutPage />
+        </Route>
+        <Route exact path="/">
+          <HomePage />
+        </Route>
     </Switch>
     </div>
     </Router>
