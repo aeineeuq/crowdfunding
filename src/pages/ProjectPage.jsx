e0308c37fc9965ccb9ce4664754596adecafe5cb
qambatali <q.ambatali@gmail.com>
2021-11-06 08:43:07 +1000

    Project Page styling updates

--- src/pages/ProjectPage.jsx
+++ src/pages/ProjectPage.jsx
@@ -1,11 +1,12 @@
 import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
+import PledgeButton from '../components/PledgeButton';
+
 
 function ProjectPage() {
     const [projectData, setProjectData] = useState({ pledges: [] });
     const { id } = useParams();
-    // const history = useHistory
-
+    
     useEffect(() => {
         fetch(`${process.env.REACT_APP_API_URL}projects/${id}`).then((results) => {
         return results.json();
@@ -16,41 +17,32 @@ function ProjectPage() {
 
     return (
         <div>
-            <div>
+            <div class="aside">
                 <img src={projectData.image} />
+                <PledgeButton />
             </div>
         <div>
-        <h2>{projectData.title}</h2>
-        <h3>Created at: {projectData.date_created}</h3>
-        <h3>Created by: {projectData.owner}</h3>
-        <h3>{`Status: ${projectData.is_open}`}</h3>
-        <p>{projectData.description}</p>
-                <h3>Pledges:</h3>
-        <ul>
-            {projectData.pledges.map((pledgeData, key) =>{
-                return (
-                <li>
-               $ {pledgeData.amount} from {pledgeData.supporter} and says {pledgeData.comment}
-                </li>
-                );
-            })}
-            </ul>
+        <div class="bside">
+            <h2>{projectData.title}</h2>
+            <h3>Created at: {projectData.date_created}</h3>
+            <h3>Created by: {projectData.owner} </h3>
+            <h3>{`Status: ${projectData.is_open}`}</h3>
+            <p>{projectData.description}</p>
+                    <h3>Pledges:</h3>
+            <ul>
+                {projectData.pledges.map((pledgeData, key) =>{
+                    return (
+                    <li>
+                $ {pledgeData.amount} from {pledgeData.supporter} and says {pledgeData.comment}
+                    </li>
+                    );
+                })}
+                </ul>
+        </div>
+        </div>
+        </div>
 
-            </div>
-            </div>
         );
-    }
-
-
-    // const deleteProject = async () => {
-    //     await fetch(`${process.env.REACT_APP_API_URL}projects/${project_id}`,{
-    //         method: "delete",
-    //         headers: {
-    //             "Authorization": `Token ${localStorage.getItem(getItem('token'))}`
-    //         }
-    //     })
-    //     history.pushState('/')
-    // }
-    
+    }    
 
     export default ProjectPage;
