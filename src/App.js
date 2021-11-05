a329b6394b626c8b37d3ff3a5a5a606fcfa4d5a9
qambatali <q.ambatali@gmail.com>
2021-11-06 08:43:48 +1000

    Update login/out function

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
-import React from "react";
-import "./App.css";
-import "./App.css";
-import { BrowserRouter as Router, Switch, Route }from"react-router-dom";
-import Nav from "./components/Nav/Nav";
-import HomePage from "./pages/HomePage";
-import LoginPage from "./pages/LoginPage";
-import LogoutPage from "./pages/LogoutPage";
-import ProjectPage from "./pages/ProjectPage";
-import UserPage from "./pages/UserPage";
+import React from 'react';
+import './App.css';
+import { BrowserRouter as Router, Switch, Route }from 'react-router-dom';
+import Nav from './components/Nav/Nav';
+import HomePage from './pages/HomePage';
+import LoginPage from './pages/LoginPage';
+import ProjectPage from './pages/ProjectPage';
+import UserPage from './pages/UserPage';
+import CreateProjectPage from './pages/CreateProjectPage'
+import Register from './pages/RegisterPage'
 import EditProjectPage from './pages/EditProjectPage';
+import PostPledge from './pages/PostPledge';
+import Footer from './components/Nav/Footer';
 
 function App() {
   return (
@@ -20,19 +22,30 @@ function App() {
         <Route path="/project/:id">
           <ProjectPage />
         </Route>
+        <Route path="/editproject/:id">
+            <EditProjectPage />
+          </Route>
         <Route path="/users/:id">
-          <UserPage />
+            <UserPage />
+        </Route>
+        <Route exact path='/createproject'>
+            <CreateProjectPage />
+        </Route>
+        <Route exact path='/postpledge'>
+            <PostPledge />
+        </Route>
+        <Route exact path='/register'>
+            <Register />
         </Route>
         <Route path="/login">
           <LoginPage />
-        </Route>
-        <Route path="/logout">
-          <LogoutPage />
         </Route>
         <Route exact path="/">
           <HomePage />
         </Route>
     </Switch>
+
+    <Footer />
     </div>
     </Router>
   );
