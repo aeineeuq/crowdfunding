87dcad02947ccf8672f9fc18fb1055d24e823e56
qambatali <q.ambatali@gmail.com>
2021-11-08 09:33:47 +1000

    Footer updates

--- src/components/Nav/Footer.jsx
+++ src/components/Nav/Footer.jsx
@@ -1,8 +1,9 @@
 import React from "react";
+import './Nav.css';
 
 function Footer() {
     return (
-        <div className="Footer">
+        <div className="footer">
             <div>
             <ul>Pawtreon - The platform for animal lovers to post projects and gain funding support</ul>
             <ul><p>&copy; Q Ambatali 2021</p></ul>
