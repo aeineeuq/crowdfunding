2fd66c8d9bd0561d7d6ca79aefa4f04ceb4a6354
qambatali <q.ambatali@gmail.com>
2021-11-08 08:43:46 +1000

    Update Footer.jsx

--- src/components/Nav/Footer.jsx
+++ src/components/Nav/Footer.jsx
@@ -4,7 +4,8 @@ function Footer() {
     return (
         <div className="Footer">
             <div>
-            <p>&copy; Q Ambatali 2021</p>
+            <ul>Pawtreon - The platform for animal lovers to post projects and gain funding support</ul>
+            <ul><p>&copy; Q Ambatali 2021</p></ul>
             </div>
         </div>
     );
