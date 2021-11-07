b11450835a78a76b636c34ef70593e7531f46c14
qambatali <q.ambatali@gmail.com>
2021-11-07 14:30:21 +1000

    Minor updates

--- src/App.js
+++ src/App.js
@@ -8,7 +8,6 @@ import LoginPage from './pages/LoginPage';
 import ProjectPage from './pages/ProjectPage';
 import CreateProjectPage from './pages/CreateProjectPage'
 import Register from './pages/RegisterPage'
-import EditProjectPage from './pages/EditProjectPage';
 
 function App() {
   return (
@@ -17,26 +16,21 @@ function App() {
      <Nav />
    
     <Switch>
-        <Route path="/project/:id">
-          <ProjectPage />
-        </Route>
-
-        
-        <Route path="/editproject/:id">
-            <EditProjectPage />
-          </Route>
-        {/* <Route path="/users/:id">
-            <UserPage />
-        </Route> */}
-        <Route exact path='/createproject'>
-            <CreateProjectPage />
-        </Route>
         <Route exact path='/register'>
             <Register />
         </Route>
         <Route path="/login">
           <LoginPage />
         </Route>
+        <Route exact path='/createproject'>
+            <CreateProjectPage />
+        </Route>
+        <Route path="/project/:id">
+          <ProjectPage />
+        </Route>       
+        {/* <Route path="/users/:id">
+            <UserPage />
+        </Route> */}
         <Route exact path="/">
           <HomePage />
         </Route>
